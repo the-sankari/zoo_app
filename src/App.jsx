8db34cc0ab0a13299345390b820efc1b45dded1f
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-05-07 20:18:50 +0300

    Header initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
   return (
-    <>
-    <h1>Zoo App</h1>
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
